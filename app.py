ec6069c0747d6ec5c59e7da34e4ef87b24534ed1
Rawa2001 <132038153+Rawa2001@users.noreply.github.com>
2025-03-19 04:42:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def index():
     return render_template('index.html')
 
 @app.route('/home')
-def index():
+def home():
     return render_template('home.html')
 
 @app.route('/track', methods=['POST'])
